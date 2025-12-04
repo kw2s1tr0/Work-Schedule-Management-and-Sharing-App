9f409bf9b5f51a2f9c7adcecd195045e1ad6cc5b
unknown <hogehoge@mail.com>
2025-12-04 21:19:49 +0900

    変更

--- front/next.config.ts
+++ front/next.config.ts
@@ -2,14 +2,6 @@ import { watch } from "fs";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config, {isServer}) => {
-    if (!isServer) {
-      config.watchOptions = {
-        poll: 1000,
-      };
-    }
-    return config;
-  }
   /* config options here */
 };
 
