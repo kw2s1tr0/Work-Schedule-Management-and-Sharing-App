f8cabfc6cbffeb674d4e5d90fc7722455ca0d641
unknown <hogehoge@mail.com>
2025-12-04 20:56:47 +0900

    ホットリロードが効かない問題解消

--- front/next.config.ts
+++ front/next.config.ts
@@ -1,6 +1,15 @@
+import { watch } from "fs";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  webpack: (config, {isServer}) => {
+    if (!isServer) {
+      config.watchOptions = {
+        poll: 1000,
+      };
+    }
+    return config;
+  }
   /* config options here */
 };
 
